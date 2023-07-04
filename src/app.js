547a83b980c4e78cb0e8902abd6b62952aec2bd1
sofi-rv <128938425+sofi-rv@users.noreply.github.com>
2023-07-04 04:53:07 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const arregloAleatorio = arreglo => {
+  let length = arreglo.length; //guardando la longitud del arreglo
+  let indice = Math.floor(Math.random() * length); //escogía un índice aleatorio
+  let planeta = arreglo[indice];
+  return planeta;
+};
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let aleatorioWho = arregloAleatorio(who);
+  let aleatorioAction = arregloAleatorio(action);
+  let aleatorioWhat = arregloAleatorio(what);
+  let aleatorioWhen = arregloAleatorio(when);
+  console.log(
+    arregloAleatorio([
+      aleatorioWho,
+      +"" + aleatorioAction,
+      +"" + aleatorioWhat,
+      +"" + aleatorioWhen
+    ])
+  );
 };
